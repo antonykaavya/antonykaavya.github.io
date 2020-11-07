89b192766f6f41759519831d496a5331b8f5f2b8
Kaavya Antony <antonykaavya@gmail.com>
2020-11-06 20:37:03 -0500

    Updates

---
+++ precache-manifest.d203e902132405c296cd9fb265076a39.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "777dbaa434a12df4681e98ad625cb778",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5db6eff968f3173e39c2",
+    "url": "/static/css/2.1afe660f.chunk.css"
+  },
+  {
+    "revision": "6bb59537e957608270cc",
+    "url": "/static/css/main.401404aa.chunk.css"
+  },
+  {
+    "revision": "5db6eff968f3173e39c2",
+    "url": "/static/js/2.d87b7ceb.chunk.js"
+  },
+  {
+    "revision": "42780b3dfc4008075422892f0026a2f6",
+    "url": "/static/js/2.d87b7ceb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bb59537e957608270cc",
+    "url": "/static/js/main.e333e759.chunk.js"
+  },
+  {
+    "revision": "9734eb3cba0fa9d5ef84",
+    "url": "/static/js/runtime-main.64fa3b4a.js"
+  },
+  {
+    "revision": "59042a26f2e843c8cc44a53f7fdb4406",
+    "url": "/static/media/headshot.59042a26.png"
+  },
+  {
+    "revision": "fe3fee8886c45d7974a241796a67ccb8",
+    "url": "/static/media/logo_big.fe3fee88.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/antonykaavya.github.io/precache-manifest.3ab1c4354a5291396ffaa6db1e4b752a.js"
+  "/precache-manifest.d203e902132405c296cd9fb265076a39.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/antonykaavya.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
